c8829ee54c33865c7f7030ac26d7bab11e95365b
DeadPackets <b00073615@aus.edu>
2020-11-05 21:34:23 +0400

    Static assets

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # Library imports
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/assets', static_folder='assets')
 
 
 # Flask routes
 @app.route('/')
 def main():
     return render_template('main.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
